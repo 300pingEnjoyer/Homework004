c9bde5712c83c12ba92e4f07816ebc3e39612616
300pingEnjoyer <va5gromov@yandex.ru>
2022-11-25 06:50:26 +0700

    task 003 complete

---
+++ 003/Program.cs
@@ -0,0 +1,25 @@
+﻿int [] CreateRandomArray (int size, int minValue, int maxValue)
+{
+    int [] newArray = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return newArray;
+}
+void ShowArray (int[] array)
+{
+    Console.Write("Получившийся массив -> ");
+   for (int i = 0; i < array.Length; i++)
+   {
+    Console.Write(array[i]+ " ");
+   } 
+}
+Console.WriteLine("Введите размер массива");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение элемента");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение массива");
+int max = Convert.ToInt32(Console.ReadLine());
+int [] myArray = CreateRandomArray(a,min,max);
+ShowArray(myArray);
