67722d8557ca4cb0699fa68307754aedd7fe6135
300pingEnjoyer <va5gromov@yandex.ru>
2022-11-25 06:42:55 +0700

    task 002 complete

---
+++ 002/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("input number");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (num > 0)
+{                       
+    int ed = num % 10; 
+    num = num / 10;  
+    sum = sum + ed;
+}
+Console.WriteLine("сумма числа = " +sum);
