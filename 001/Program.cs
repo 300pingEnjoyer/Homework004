d3b846b338e1edd0308ca7a91ca785e7c3f55988
300pingEnjoyer <va5gromov@yandex.ru>
2022-11-25 06:20:55 +0700

    task 001 complete

---
+++ 001/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("input numA");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input numB");
+int numB = Convert.ToInt32(Console.ReadLine());
+int step = numA;
+
+for (int i = 1; i < numB; i++)
+{
+    step = step * numA;
+}
+
+Console.WriteLine("число A в степени B равно = " +step);
+
+
